29dff64dfab5e3cf00738c9191cd3686c6e907ee
marianalopesdev <marianalopes.dev@gmail.com>
2023-02-12 11:49:24 -0300

    Updates

--- data.js
+++ data.js
@@ -1,9 +1,39 @@
-// estas funciones son de ejemplo
+export const films = {
 
-export const example = () => {
-  return 'example';
-};
+  alphabeticOrderFilter: function alphabeticOrderFilter(dataFilms) {    
+    return dataFilms.sort(function(a, b) {
+      if (a.title < b.title) return -1;
+      if (a.title > b.title) return 1;
+      return 0;      
+    });
+    
+  }, 
 
-export const anotherExample = () => {
-  return 'OMG';
+  inverseAlphabeticOrderFilter: function inverseAlphabeticOrderFilter(dataFilms) {
+
+    return dataFilms.sort((a, b) => b.title.localeCompare(a.title));    
+    
+  },
+  searchFilms: function searchFilms(){
+    const input = document.getElementById("filter").value.toUpperCase();
+    //Chamar cards do HTML
+    const cards = document.getElementsByClassName("cards");
+  
+    //Iterar por todos os cards e selecionar os títulos pelo index de cada um
+    for (let i = 0; i < cards.length; i++) {
+      const titles = cards[i].querySelector("#film-title");
+      //console.log(titles);
+  
+      //Checar o texto do título em uppercase e o index de input no array 
+      if(titles.innerText.toUpperCase().indexOf(input) > -1){
+        //Retornar o card do array se o index for válido, ou seja, existir no array (igual ou maior que 0)
+        cards[i].style.display = "";
+      } else {
+        //Se o index for inválido, o display será nulo, e nada aparecerá
+        cards[i].style.display = "none";
+      }
+  
+      //console.log(cards);
+    }
+  }
 };
